b8bdb47f9839c7035fcbdda62b1981338842fa0f
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-11-24 14:59:17 +0000

    added html for default page, created scss files, added js pseudocode

---
+++ script.js
@@ -0,0 +1,22 @@
+// Mobile
+// User enters a task in the input box
+// User clicks the "+" button to submit the task, which shows up in a list under the input box
+// No. of tasks they add gets updated in "Tasks to complete: n"
+// User can click checks to tick of tasks
+// When they do so a banner telling them the no. of tasks they have selected and a delete button appears at the footer of the page
+// When they have no tasks to complete - text saying this shows on the page
+
+// functions which need event listeners
+// reset - will reset page to have no tasks added
+// add button for task input - will 'submit' the tasks entered by the user and dynamically change the page to list the submissions and add a p tag saying the no. of tasks to complete
+// empty box next to tasks- users can fill these buttons with ticks which changes the page to add a banner with a delete button
+
+// Query selectors
+const resetPage = document.querySelector(".nav__reset");
+const taskSubmit = document.querySelector(".task__adder");
+
+const handleSubmit = () => {
+
+}
+
+taskSubmit.addEventListener("click", handleSubmit);
